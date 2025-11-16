95e5c723b8d3f2fcc32bf92e48559972b74349d3
FLYCOM-E <flycome9798@gmail.com>
2025-11-16 10:43:13 +0800

    Update ?

--- service.sh
+++ service.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+;#!/system/bin/sh
 MODDIR=${0%/*}
 cd "$MODDIR"
 exec 2>>"$MODDIR/LOG.log"
@@ -7,25 +7,24 @@ exec 2>>"$MODDIR/LOG.log"
 echo "<$(date)> ReStart" > "$MODDIR/LOG.log"
 
 # Wait boot=1
-while true; do
-    [ -d "/storage/emulated/0/Android/data" ] && break
-sleep 5
+while [ ! -d "/storage/emulated/0/Android/data" ]; do
+    sleep 5
 done
 
 # Off SELinux
 [ "$(getenforce)" = "Enforcing" ] && setenforce 0 && OffSelinux=1
 
 # Server
-if $(pm list package -3 | grep moe.shizuku.privileged.api >>/dev/null); then
-    
+if pm list package -3 | grep moe.shizuku.privileged.api >/dev/null 2>&1; then
     # Get Shizuku path
     path=$(pm path moe.shizuku.privileged.api | sed 's|package:||; s|/base.apk||')
     abi=$(getprop ro.system.product.cpu.abilist | cut -f1 -d '-')
-    
     # Run Shizuku
     if [ -d "$path" ]; then
-        if $("$path/lib/$abi/libshizuku.so" >>/dev/null); then
+        if "$path/lib/$abi/libshizuku.so" >/dev/null 2>&1; then
             echo "AutoShizuku Server: Shizuku is Run." > "$MODDIR/LOG.log"
+        else
+            echo "AutoShizuku Server: Start Shizuku Error." > "$MODDIR/LOG.log"
         fi
     else
         echo "AutoShizuku Server: Shizuku PATH not find. Version Not Ok? " > "$MODDIR/LOG.log"
